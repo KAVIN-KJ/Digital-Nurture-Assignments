34e2d52582b37d0f332fbb3a548d2f69c0a1f78b
KAVIN-KJ <kavinkj2004@gmail.com>
2025-07-06 21:47:07 +0530

    Finish Week 3

--- Week_3/Spring_Core_And_Maven/Exercise4/src/main/java/com/library/LibraryManagementApplication.java
+++ Week_3/Spring_Core_And_Maven/Exercise4/src/main/java/com/library/LibraryManagementApplication.java
@@ -1,4 +1,4 @@
-package com.library;
+package Week_3.Spring_Core_And_Maven.Exercise4.src.main.java.com.library;
 
 public class LibraryManagementApplication {
     public static void main(String[] args) {
--- Week_3/Spring_Core_And_Maven/Exercise4/src/test/java/com/library/AppTest.java
+++ Week_3/Spring_Core_And_Maven/Exercise4/src/test/java/com/library/AppTest.java
@@ -1,4 +1,4 @@
-package com.library;
+package Week_3.Spring_Core_And_Maven.Exercise4.src.test.java.com.library;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
